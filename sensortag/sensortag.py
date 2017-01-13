1ce89367f1fee563312e7ed298c9ef69cbd81b5a
Michael Saunby <mike@saunby.net>
2017-01-13 19:54:59 +0000

    with commands

--- sensortag/sensortag.py
+++ sensortag/sensortag.py
@@ -25,6 +25,16 @@ import time
 from sensor_calcs import *
 import json
 import select
+from tag_commands import readCommand
+
+DEBUG = False
+
+tag = None
+
+def debug_print(str):
+    if DEBUG:
+        print(str)
+
 
 def floatfromhex(h):
     t = float.fromhex(h)
@@ -38,7 +48,7 @@ class SensorTag:
     def __init__( self, bluetooth_adr ):
         self.con = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
         self.con.expect('\[LE\]>', timeout=600)
-        print("Preparing to connect. You might need to press the side button...")
+        debug_print("Preparing to connect. You might need to press the side button...")
         self.con.sendline('connect')
         # test for success of connect
         self.con.expect('Connection successful.*\[LE\]>')
@@ -51,10 +61,61 @@ class SensorTag:
         self.cb = {}
         return
 
+    def enableSensors( self, sensors ):
+        print ("Enabling", sensors)
+        global tag
+        cbs = tag.cbs
+        if True:
+              # enable IR sensor
+              tag.register_cb(0x21,cbs.ir)
+              tag.char_write_cmd(0x24, "01")   # IR Temperature Config  RW  Write "01" to start Sensor and Measurements, "00" to put to sleep
+              tag.char_write_cmd(0x22, "0100") # Client Characteristic Configuration  RW  Write "01:00" to enable notifications, "00:00" to disable
+
+              # enable humidity
+              tag.register_cb(0x29, cbs.humidity)
+              tag.char_write_cmd(0x2C, "01") # Humidity Config		RW	Write "01" to start measurements, "00" to stop
+              tag.char_write_cmd(0x2A,"0100") # Client Characteristic Configuration		RW	Write "01:00" to enable notifications, "00:00" to disable
+
+              # enable lux
+              tag.register_cb(0x41, cbs.lux)
+              tag.char_write_cmd(0x44, "01") # Luxometer Config		RW	Write "01" to start Sensor and Measurements, "00" to put to sleep
+              tag.char_write_cmd(0x42,"0100") # Client Characteristic Configuration		RW	Write "01:00" to enable notifications, "00:00" to disable
+
+              # enable barometer
+              tag.register_cb(0x31,cbs.baro)
+              tag.char_write_cmd(0x34, "01") # Write "01" to start Sensor and Measurements, "00" to put to sleep, "02" to read calibration values from sensor
+              tag.char_write_cmd(0x32, "0100") # Write "01:00" to enable notifications, "00:00" to disable
+
+    def disableSensors( self, sensors ):
+        print ("Disabling", sensors)
+        global tag
+        # disable IR sensor
+        tag.char_write_cmd(0x24, "00")
+        # disable humidity sensor
+        tag.char_write_cmd(0x2C, "00")
+        # disable lux sensor
+        tag.char_write_cmd(0x44, "00")
+        # disable barometer sensor
+        tag.char_write_cmd(0x34, "00")
+
+    def setInterval( self, ms ):
+        if ms < 1000:
+            ms = 1000
+        hexinterval = "%04X" % int(ms/10)
+        # IR
+        tag.char_write_cmd(0x26, hexinterval)
+        # humidity
+        tag.char_write_cmd(0x2E, hexinterval)
+        # lux
+        tag.char_write_cmd(0x46, hexinterval)
+        # barometer
+        tag.char_write_cmd(0x36, hexinterval)
+
+
     def char_write_cmd( self, handle, value ):
         # The 0%x for value is VERY naughty!  Fix this!
         cmd = 'char-write-cmd 0x%02x %s' % (handle, value)
-        print (cmd)
+        debug_print (cmd)
         self.con.sendline( cmd )
         return
 
@@ -68,17 +129,31 @@ class SensorTag:
     # Notification handle = 0x0025 value: 9b ff 54 07
     def notification_loop( self ):
         while True:
+            pnum = None
+            handle = None
+            a = select.select([sys.stdin], [], [], 0)
+            if a[0]:
+                text = sys.stdin.readline()
+                (kind,value) = readCommand(text)
+                print (kind, value)
+                if kind == "QUIT":
+                    sys.exit(0)
+                elif kind == "ENABLE":
+                    self.enableSensors(value)
+                elif kind == "DISABLE":
+                    self.disableSensors(value)
+                elif kind == "INTERVAL":
+                    self.setInterval(int(value[0]))
             try:
               pnum = self.con.expect('Notification handle = .*? \r', timeout=4)
             except pexpect.TIMEOUT:
               print ("TIMEOUT exception!")
-              break
+              #break
             if pnum==0:
                 after = self.con.after
                 hxstr = after.split()[3:]
                 handle = int(float.fromhex(hxstr[0].decode("utf-8")))
-            	#try:
-            if True:
+            #if handle:
                 self.cb[handle]([int(float.fromhex(n.decode("utf-8"))) for n in hxstr[2:]])
             	#except:
                 #  print "Error in callback for %x" % handle
@@ -106,81 +181,51 @@ class SensorCallbacks:
         ambT = (v[3]<<8)+v[2]
         (targetT, ambientT) = calcTmpTarget(objT, ambT)
         self.data['ir'] = [targetT, ambientT]
-        print ("IR %.1f %.1f" % (targetT, ambientT))
+        debug_print ("IR %.1f %.1f" % (targetT, ambientT))
 
     def accel(self,v):
         (xyz,mag) = calcAccel(v[0],v[1],v[2])
         self.data['accl'] = xyz
-        print ("ACCL", xyz)
+        debug_print ("ACCL", xyz)
 
     def humidity(self, v):
         rawT = (v[1]<<8)+v[0]
         rawH = (v[3]<<8)+v[2]
         (t, rh) = calcHum(rawT, rawH)
         self.data['humd'] = [t, rh]
-        print ("HUMD %.1f %.1f" % (t, rh))
+        debug_print ("HUMD %.1f %.1f" % (t, rh))
 
     def lux(self, v):
         rawL = (v[1]<<8)+v[0]
         lux = calcLux(rawL)
         self.data['lux'] = lux
-        print ("LUX %.1f" % lux)
+        debug_print ("LUX %.1f" % lux)
 
     def baro(self,v):
         rawT = (v[2]<<16)+(v[1]<<8)+v[0]
         rawP = (v[5]<<16)+(v[4]<<8)+v[3]
         (temp, pres) =  calcBaro(rawT, rawP)
         self.data['baro'] = [temp, pres]
-        print ("BARO %0.1f %0.1f" % (temp, pres))
+        debug_print ("BARO %0.1f %0.1f" % (temp, pres))
         self.data['time'] = int(time.time() * 1000)
         print (self.data)
 
 
 def main():
-    global datalog
+    global datalog, tag
     bluetooth_adr = sys.argv[1]
     if len(sys.argv) > 2:
         datalog = open(sys.argv[2], 'w+')
 
     while True:
-     #try:
-         #pass
-
-     while True:
-      print ("[re]starting..")
+      debug_print ("[re]starting..")
 
       tag = SensorTag(bluetooth_adr)
-      cbs = SensorCallbacks(bluetooth_adr)
-
-      # enable TMP006 sensor
-      tag.register_cb(0x21,cbs.ir)
-      tag.char_write_cmd(0x24, "01")   # IR Temperature Config  RW  Write "01" to start Sensor and Measurements, "00" to put to sleep
-      tag.char_write_cmd(0x26, "20") # IR Temperature Period  RW  Period = [Input*10] ms, (lower limit 300 ms), default 1000 ms
-      tag.char_write_cmd(0x22, "0100") # Client Characteristic Configuration  RW  Write "01:00" to enable notifications, "00:00" to disable
-
-
-      # enable humidity
-      tag.register_cb(0x29, cbs.humidity)
-      tag.char_write_cmd(0x2C, "01") # Humidity Config		RW	Write "01" to start measurements, "00" to stop
-      tag.char_write_cmd(0x2E,"40") # Humidity Period		RW	Period = [Input*10] ms, (lower limit 100 ms), default 1000 ms
-      tag.char_write_cmd(0x2A,"0100") # Client Characteristic Configuration		RW	Write "01:00" to enable notifications, "00:00" to disable
-
-      # enable lux
-      tag.register_cb(0x41, cbs.lux)
-      tag.char_write_cmd(0x44, "01") # Luxometer Config		RW	Write "01" to start Sensor and Measurements, "00" to put to sleep
-      tag.char_write_cmd(0x46,"80") # Luxometer Period		RW	Period = [Input*10]ms (lower limit 1000ms), default 2000ms
-      tag.char_write_cmd(0x42,"0100") # Client Characteristic Configuration		RW	Write "01:00" to enable notifications, "00:00" to disable
-
-      # enable barometer
-      tag.register_cb(0x31,cbs.baro)
-      tag.char_write_cmd(0x32, "0100") # Write "01:00" to enable notifications, "00:00" to disable
-      tag.char_write_cmd(0x34, "01") # Write "01" to start Sensor and Measurements, "00" to put to sleep, "02" to read calibration values from sensor
-      tag.char_write_cmd(0x36, "40") # Barometer Period		RW	Period = [Input*10] ms, (lower limit 100 ms), default 1000 ms
+      tag.cbs = SensorCallbacks(bluetooth_adr)
 
 
       tag.notification_loop()
-     #except:
-      #pass
+
 
 if __name__ == "__main__":
     main()
